20c28239bcd3077da54320efb15d51e48c4c8730
Hiren Bambhaniya <148859956+HirenCodeMaster11@users.noreply.github.com>
2024-08-24 19:13:44 +0530

    Update Prime.java

--- src/Prime.java
+++ src/Prime.java
@@ -1,7 +1,19 @@
-public class Prime {
-    public static void main(String[] arg)
-    {
-        int i,j;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int sum = 0;
+        System.out.print("Enter value: ");
+        a = scanner.nextInt();
+
+        for (int i = 1; i < val; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0) {
+                System.out.println(i);
+                sum += i;
+            }
+        }
+        System.out.println("Sum: " + sum);
     }
 }
